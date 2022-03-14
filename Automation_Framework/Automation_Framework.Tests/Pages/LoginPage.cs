a631ed3b72acffa69b13f2e218d6707acfa25a59
KhalidEM <Khalid.Em@hotmail.com>
2022-03-14 09:41:22 +0100

    login registerBTN changes xpath

--- Automation_Framework/Automation_Framework.Tests/Pages/LoginPage.cs
+++ Automation_Framework/Automation_Framework.Tests/Pages/LoginPage.cs
@@ -15,7 +15,7 @@ namespace Automation_Framework.Tests.Pages
         {
         }
         public InputField SignInEmail => new InputField(Driver, By.XPath("//input[@id='SignInEmail']"));
-        public InputField SignInPassword => new InputField(Driver, By.XPath("//input[@id='SignInEmail']"));
+        public InputField SignInPassword => new InputField(Driver, By.XPath("//input[@id='SignInPassword']"));
         public Button SignInButtonComplete => new Button(Driver, By.XPath("//button[@id='SignInButtonComplete']"));
         /*public IWebElement SignInEmail => Driver.FindElement(By.XPath("//input[@id='SignInEmail']"));
         public IWebElement SignInPassword => Driver.FindElement(By.XPath("//input[@id='SignInPassword']"));

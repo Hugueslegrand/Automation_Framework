928dfca0a32011e5e80dfd952d4266b78da121c9
KhalidEM <Khalid.Em@hotmail.com>
2022-03-30 15:09:28 +0200

    Update MyMovies

--- Automation_Framework/Automation_Framework.Tests/Pages/MyMoviesPage.cs
+++ Automation_Framework/Automation_Framework.Tests/Pages/MyMoviesPage.cs
@@ -14,8 +14,16 @@ namespace Automation_Framework.Tests.Pages
         
 
         public IButton MovieTitle => new WebElement(Driver, "//p[@class='css-49l3oe']", Selector.Xpath);
-
         public IButton MovieAvailableDate => new WebElement(Driver, "//p[@class='css-1l8ljr1']", Selector.Xpath);
+        public IButton MovieHoverPoster => new WebElement(Driver, "//img[@class='testing css-8cfhcr']", Selector.Xpath);
+        public IButton MovieHoverTitle => new WebElement(Driver, "//p[@class='css-49l3oe']", Selector.Xpath);
+        public IButton MovieHoverDescription => new WebElement(Driver, "//p[@class='css-1sivm4u']", Selector.Xpath);
+        public IButton MovieHoverActorImage => new WebElement(Driver, "//img[@class='css-8ut8n4']", Selector.Xpath);
+        //public IButton MovieHoverRealName => new WebElement(Driver, "//p[@class='css-1sivm4u']", Selector.Xpath);
+        //public IButton MovieHoverActorName => new WebElement(Driver, "//p[@class='css-1sivm4u']", Selector.Xpath);
+
+
+
         public IButton WatchNowButton => new WebElement(Driver, "//button[normalize-space()='Watch now']", Selector.Xpath);
         public IButton CloseModalButton => new WebElement(Driver, "//div[@class='css-ce9ngx']//button[@id='CloseModal']",Selector.Xpath);
 

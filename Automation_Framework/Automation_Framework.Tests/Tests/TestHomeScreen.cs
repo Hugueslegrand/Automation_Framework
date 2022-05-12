5af8dfc31088a3c2e2bcc0dd74ba75cb34bf122b
Ismailov Ibragim (s124306) <ibragim.ismailov@student.ap.be>
2022-05-12 19:31:38 +0200

    Set-Up test 4 genres

---
+++ Automation_Framework/Automation_Framework.Tests/Tests/TestHomeScreen.cs
@@ -0,0 +1,24 @@
+﻿using Automation_Framework.Tests.Models;
+using Automation_Framework.Tests.Pages;
+using FluentAssertions;
+using NUnit.Framework;
+
+
+namespace Automation_Framework.Tests.Tests
+{
+    public class TestHomeScreen : BaseTest
+    {
+        User userAdminExist = new User("Stage", "Admin", "stageadmin@stageadmin.stageadmin", "StageAdmin0221!");
+
+        [Test, Property("caseid", "7292")]
+        [Description("Verify the homepage contains 4 selections by genre of differen movies")]
+        public void Test_AvailableGenres_HomePage()
+        {
+            
+
+        }
+
+        
+
+    }
+}
